f5e67cc762b931cb20409b1b084048871a8eca51
juhyeon1114 <juhyeon1114@gmail.com>
2025-09-02 21:41:48 +0900

    오타 수정

--- src/main/java/com/system/batch/killbatch/KillBatchApplication.java
+++ src/main/java/com/system/batch/killbatch/KillBatchApplication.java
@@ -1,4 +1,4 @@
-package com.systen.batch.killbatch;
+package com.system.batch.killbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/system/batch/killbatch/KillBatchApplicationTests.java
+++ src/test/java/com/system/batch/killbatch/KillBatchApplicationTests.java
@@ -1,4 +1,4 @@
-package com.systen.batch.killbatch;
+package com.system.batch.killbatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
